1f2aff9b5419bd1b62ec5d959c05bf75897738fc
Jhan Khendrick Perez <jhankhendrickperez@gmail.com>
2026-02-09 12:08:11 +0800

    SOAP ADDING COLORS: Added ColorData to hold its Value

---
+++ Assets/Scripts/ScriptableScripts/ColorDataScriptableObject.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Color Data", menuName = "Color Data/New Color SO")]
+public class ColorDataScriptableObject : ScriptableObject
+{
+    public ColorPaletteValue Value;
+}
