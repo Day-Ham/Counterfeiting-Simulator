11ffbec6cd734eb30211d67544ca32db30810f6d
Jhan Khendrick Perez <jhankhendrickperez@gmail.com>
2026-03-02 10:12:12 +0800

    MT#16: Scale Slider Snap, Drag, Scrollwheel

--- Assets/Scripts/ScaleSlider.cs
+++ Assets/Scripts/ScaleSlider.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using DaeHanKim.ThisIsTotallyADollar.Drawing;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 public class ScaleSlider : MonoBehaviour
 {
     public Slider BrushScaleSlider;
     public CanvasDrawControllerValue BrushController;
     public GameObjectValue Cursor;
-
+    
+    public RectTransform SliderRect;
+    public Canvas ParentCanvas;
+    
     public float ReferenceNumber = 64;
-
+    public float ScrollSensitivity;
+    
     private CanvasDrawController CanvasDrawController => BrushController.Value;
     
     private void Awake()
@@ -28,6 +33,24 @@ public class ScaleSlider : MonoBehaviour
             BrushScaleSlider.onValueChanged.RemoveListener(SetSize);
         }
     }
+    
+    private void Update()
+    {
+        // Check if mouse is inside full slider area
+        if (!RectTransformUtility.RectangleContainsScreenPoint(SliderRect, Input.mousePosition, ParentCanvas.worldCamera))
+        {
+            return;
+        };
+        
+        float scroll = Input.mouseScrollDelta.y;
+
+        if (Mathf.Abs(scroll) > 0.01f)
+        {
+            float newValue = BrushScaleSlider.value + scroll * ScrollSensitivity;
+            BrushScaleSlider.value = Mathf.Clamp(newValue, BrushScaleSlider.minValue, BrushScaleSlider.maxValue);
+        }
+    }
+    
     
     private void SetSize(float brushScaleSize)
     {
