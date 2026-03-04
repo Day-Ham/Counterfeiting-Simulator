ebcd0a41132e0ed167007edf47769ae47efebdde
Jhan Khendrick Perez <jhankhendrickperez@gmail.com>
2026-03-04 12:24:50 +0800

    MT#21: Prefab Templates for Levels is Intatiated

--- Assets/Scripts/LevelConfig/LevelManager.cs
+++ Assets/Scripts/LevelConfig/LevelManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
@@ -5,9 +6,21 @@ public class LevelManager : MonoBehaviour
     [Header("Level Settings")]
     [SerializeField] private LevelConfig _levelToLoad;
     [SerializeField] private LevelConfigRuntimeAsset _levelConfigRuntimeAsset;
+    
+    [Header("Prefabs to Instantiate")]
+    [SerializeField] private List<GameObject> _prefabsToSpawn;
 
     private void Awake()
     {
         _levelConfigRuntimeAsset.Value = _levelToLoad;
+        SpawnPrefabs();
+    }
+    
+    private void SpawnPrefabs()
+    {
+        foreach (GameObject prefab in _prefabsToSpawn)
+        {
+            Instantiate(prefab);
+        }
     }
 }
