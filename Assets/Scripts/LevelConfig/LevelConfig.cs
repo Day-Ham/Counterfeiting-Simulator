f6fb84b8061e789bb0bffbee2405a749b07c8a66
Jhan Khendrick Perez <jhankhendrickperez@gmail.com>
2026-03-05 16:57:36 +0800

    MT#23: Added LevelGameMode

--- Assets/Scripts/LevelConfig/LevelConfig.cs
+++ Assets/Scripts/LevelConfig/LevelConfig.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewLevelConfig", menuName = "Level/LevelConfig")]
 public class LevelConfig : ScriptableObject
 {
+    public LevelGameMode LevelGameMode;
     public CanvasTemplateValue CanvasTemplate;
     public ColorDataValue ColorBackgroundDraw;
     public GameObjectListValue ColorBlobs;
     public ColorDataListValue ColorsToBeUsed;
     public TextureValueWrapper TargetTexture;
+    
+    public LevelGameMode GameMode => LevelGameMode;
 }
---
+++ Assets/Scripts/LevelConfig/LevelGameMode.cs
@@ -0,0 +1,5 @@
+public enum LevelGameMode
+{
+    Standard,
+    ColorPicker
+}
