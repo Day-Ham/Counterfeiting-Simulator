6b6cf29d7c2c6be57fee17975e3e140edadfe4d8
Jhan Khendrick Perez <jhankhendrickperez@gmail.com>
2026-03-11 13:21:17 +0800

    MT#26: Removed ICrayonSource Interface

--- Assets/Scripts/Interfaces/ICrayonSource.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ICrayonSource
-{
-    List<Color> GetActiveColors();
-    GameObjectListValue GetColorBlobs();
-    void InitializeColors();
-}
