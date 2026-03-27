acfb45a47257164e7b067c7c56f9fdb66cb38348
Jhan Khendrick Perez <jhankhendrickperez@gmail.com>
2026-03-27 17:53:53 +0800

    MT#47: Improved TransitionCircle System

--- Assets/Scripts/UI/Button/ChangeScene.cs
+++ Assets/Scripts/UI/Button/ChangeScene.cs
@@ -6,24 +6,43 @@ using UnityEngine.UI;
 public class ChangeScene : MonoBehaviour
 {
     [Header("Events")]
-    [SerializeField] private CallbackEvent playTransitionEvent;
+    [SerializeField] private VoidEvent playTransitionEvent;
+    [SerializeField] private VoidEvent onTransitionFinished;
 
     [Header("Buttons")]
     public Button button;
 
     [Header("Scene")]
     public SingleSceneReference sceneToGo;
+    
+    private bool _shouldLoadScene;
 
     private void Awake()
     {
         button.onClick.AddListener(OnButtonClicked);
     }
+    
+    private void OnEnable()
+    {
+        onTransitionFinished.Register(OnTransitionFinished);
+    }
+
+    private void OnDisable()
+    {
+        onTransitionFinished.Unregister(OnTransitionFinished);
+    }
 
     private void OnButtonClicked()
     {
-        playTransitionEvent?.Raise(() =>
-        {
-            SceneManagerUtility.LoadScene(sceneToGo);
-        });
+        _shouldLoadScene = true;
+        playTransitionEvent.Raise();
+    }
+    
+    private void OnTransitionFinished()
+    {
+        if (!_shouldLoadScene) return;
+
+        _shouldLoadScene = false;
+        SceneManagerUtility.LoadScene(sceneToGo);
     }
 }
